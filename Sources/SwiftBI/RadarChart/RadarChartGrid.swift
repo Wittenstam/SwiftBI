8f9fb336c3f24dd82ad323cac31b3dddca4e4edb
Wittenstam <marcus.wittenstam@hotmail.com>
2021-11-11 00:59:15 +0100

    added init

--- Sources/SwiftBI/RadarChart/RadarChartGrid.swift
+++ Sources/SwiftBI/RadarChart/RadarChartGrid.swift
@@ -8,40 +8,45 @@
 import Foundation
 import SwiftUI
 
+
 public struct RadarChartGrid: Shape {
+        
+    let categories: Int
+    let divisions: Int
     
-  let categories: Int
-  let divisions: Int
-  
-  public func path(in rect: CGRect) -> Path {
+    public init(categories: Int,divisions: Int) {
+        self.categories = categories
+        self.divisions = divisions
+    }
+      
+    public func path(in rect: CGRect) -> Path {
       
     let radius = min(rect.maxX - rect.midX, rect.maxY - rect.midY)
     let stride = radius / CGFloat(divisions)
     var path = Path()
-    
+
     for category in 1 ... categories {
-      path.move(to: CGPoint(x: rect.midX, y: rect.midY))
-      path.addLine(to: CGPoint(x: rect.midX + cos(CGFloat(category) * 2 * .pi / CGFloat(categories) - .pi / 2) * radius,
+        path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.midX + cos(CGFloat(category) * 2 * .pi / CGFloat(categories) - .pi / 2) * radius,
                                y: rect.midY + sin(CGFloat(category) * 2 * .pi / CGFloat(categories) - .pi / 2) * radius
                               )
                 )
     }
-    
+
     for step in 1 ... divisions {
-      let rad = CGFloat(step) * stride
-      path.move(to: CGPoint(x: rect.midX + cos(-.pi / 2) * rad,
+        let rad = CGFloat(step) * stride
+        path.move(to: CGPoint(x: rect.midX + cos(-.pi / 2) * rad,
                             y: rect.midY + sin(-.pi / 2) * rad))
-      
-      for category in 1 ... categories {
+
+        for category in 1 ... categories {
         path.addLine(to: CGPoint(x: rect.midX + cos(CGFloat(category) * 2 * .pi / CGFloat(categories) - .pi / 2) * rad,
                                  y: rect.midY + sin(CGFloat(category) * 2 * .pi / CGFloat(categories) - .pi / 2) * rad
                                 )
                     )
-      }
-        
+        }
     }
-    
+
     return path
-  }
+    }
     
 }
--- Sources/SwiftBI/RadarChart/RadarChartPath.swift
+++ Sources/SwiftBI/RadarChart/RadarChartPath.swift
@@ -8,39 +8,46 @@
 import Foundation
 import SwiftUI
 
+
 public struct RadarChartPath: Shape {
+        
+    let data: [Double]
     
-  let data: [Double]
-  
-  public func path(in rect: CGRect) -> Path {
-    guard
-      3 <= data.count,
-      let minimum = data.min(),
-      0 <= minimum,
-      let maximum = data.max()
-    else { return Path() }
-    
-    let radius = min(rect.maxX - rect.midX, rect.maxY - rect.midY)
-    var path = Path()
-    
-    for (index, entry) in data.enumerated() {
-      switch index {
-        case 0:
-          path.move(to: CGPoint(x: rect.midX + CGFloat(entry / maximum) * cos(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius,
-                                y: rect.midY + CGFloat(entry / maximum) * sin(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius
-                               )
-                    )
-          
-        default:
-          path.addLine(to: CGPoint(x: rect.midX + CGFloat(entry / maximum) * cos(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius,
-                                   y: rect.midY + CGFloat(entry / maximum) * sin(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius
-                                  )
-                    )
-      }
+    public init(data: [Double]) {
+        self.data = data
     }
-      
-    path.closeSubpath()
-    return path
-  }
+
+    public func path(in rect: CGRect) -> Path {
+        guard
+            3 <= data.count,
+                let minimum = data.min(),
+            0 <= minimum,
+                let maximum = data.max()
+            else { return Path() }
+
+        let radius = min(rect.maxX - rect.midX, rect.maxY - rect.midY)
+        var path = Path()
+
+        for (index, entry) in data.enumerated() {
+            switch index {
+                case 0:
+                  path.move(to: CGPoint(x: rect.midX + CGFloat(entry / maximum) * cos(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius,
+                                        y: rect.midY + CGFloat(entry / maximum) * sin(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius
+                                       )
+                            )
+                  
+                default:
+                  path.addLine(to: CGPoint(x: rect.midX + CGFloat(entry / maximum) * cos(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius,
+                                           y: rect.midY + CGFloat(entry / maximum) * sin(CGFloat(index) * 2 * .pi / CGFloat(data.count) - .pi / 2) * radius
+                                          )
+                            )
+            }
+        }
+          
+        path.closeSubpath()
+        return path
+    }
+    
+    
     
 }
