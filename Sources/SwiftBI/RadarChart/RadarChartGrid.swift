81f7432a448815e11338bf5cf7ef419e0ee942af
Wittenstam <marcus.wittenstam@hotmail.com>
2021-11-11 00:21:13 +0100

    Update RadarChartGrid.swift

--- Sources/SwiftBI/RadarChart/RadarChartGrid.swift
+++ Sources/SwiftBI/RadarChart/RadarChartGrid.swift
@@ -8,20 +8,6 @@
 import Foundation
 import SwiftUI
 
-/*
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
-    }
-}
-*/
-
 struct RadarChartGrid: Shape {
     
   let categories: Int
