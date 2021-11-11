c6c6436680a32642d2ea86573303f9e232dc0e0d
Wittenstam <marcus.wittenstam@hotmail.com>
2021-11-11 20:53:42 +0100

    added init

--- Sources/SwiftBI/RadarChart/RadarChartData.swift
+++ Sources/SwiftBI/RadarChart/RadarChartData.swift
@@ -11,6 +11,11 @@ import Foundation
 public struct RadarChartData {
      var label: String
      var value: Double
+    
+    public init(label: String, value: Double) {
+        self.label = label
+        self.value = value
+    }
  }
 
 
